8cf63fcd9c0db2c716ffad1ab103e631d4cbe568
devilcry3 <78716116+devilcry3@users.noreply.github.com>
2026-02-25 20:13:09 -0700

    Building, LocationManager, and BearingCalcultar sheets
    FUlly created and coded swift scripts
    
    Building - Holds building string syntax and latitude and longitude
    
    LocationManager - Handles permission checks, location undates, location update checks, turns automatic update shut off to on, as well as functions for detecting changes to location and heading update checks.
    
    Bearing Calculator - Handles all the math for distance and direction. Finds the lattitude and longitudes of desired location and user, then finds the difference. then uses phone position to find rotation need to face building. Then normalize the data to be a 0-360 direction. does degree to radian and vise versa math.

--- QuadNav/0_Models/Building.swift
+++ QuadNav/0_Models/Building.swift
@@ -1,26 +1,57 @@
-//
 //  Building.swift
 //  QuadNav
 //
 //  Created by Brandon Williams on 2/25/26.
-//
+//creates variable sturctured objects that can be refferenced
 
 import Foundation
 import CoreLocation
 
-struct Building {
+struct Building: Identifiable {
+    
+    let id = UUID()
     let name: String
     let coordinate: CLLocationCoordinate2D
+    
 }
+
+
+// MARK: - Static Campus Data
+
 extension Building {
+    
     static let campusBuildings: [Building] = [
-        Building(name: "Building A",
-                 coordinate: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)),
-        Building(name: "Building B",
-                 coordinate: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)),
-        Building(name: "Building C",
-                 coordinate: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)),
-        Building(name: "Building D",
-                 coordinate: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0))
+        
+        Building(
+            name: "GIMM Lab",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 43.6025,
+                longitude: -116.2034
+            )
+        ),
+        
+        Building(
+            name: "Library",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 43.6028,
+                longitude: -116.2040
+            )
+        ),
+        
+        Building(
+            name: "Student Union",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 43.6032,
+                longitude: -116.2037
+            )
+        ),
+        
+        Building(
+            name: "Science Building",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 43.6029,
+                longitude: -116.2028
+            )
+        )
     ]
 }
--- QuadNav/1_Location/LocationManager.swift
+++ QuadNav/1_Location/LocationManager.swift
@@ -5,46 +5,84 @@
 //  Created by Brandon Williams on 2/25/26.
 //
 
+import Foundation
 import CoreLocation
 import Combine
 
 final class LocationManager: NSObject,
                              ObservableObject,
                              CLLocationManagerDelegate {
-
+    
+    // MARK: - Core Location Manager
+    
     private let manager = CLLocationManager()
-
+    
+    // MARK: - Published State
+    
     @Published var location: CLLocation?
     @Published var heading: CLHeading?
     @Published var authorizationStatus: CLAuthorizationStatus?
-
+    
+    // MARK: - Initialization
+    
     override init() {
         super.init()
         manager.delegate = self
         configureManager()
     }
-
+    
+    // MARK: - Configuration (Efficient Monitoring)
+    
     private func configureManager() {
-        // Efficient Monitoring Configuration
         manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         manager.distanceFilter = 5
         manager.pausesLocationUpdatesAutomatically = true
         manager.activityType = .otherNavigation
     }
-
+    
+    // MARK: - Permission
+    
     func requestPermission() {
         manager.requestWhenInUseAuthorization()
     }
-
+    
+    // MARK: - Start / Stop Updates
+    
     func startUpdates() {
         manager.startUpdatingLocation()
         manager.startUpdatingHeading()
     }
-
+    
     func stopUpdates() {
         manager.stopUpdatingLocation()
         manager.stopUpdatingHeading()
     }
-
-    // Delegate methods implemented here
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            startUpdates()
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        location = locations.last
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateHeading newHeading: CLHeading) {
+        heading = newHeading
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
+    }
 }
