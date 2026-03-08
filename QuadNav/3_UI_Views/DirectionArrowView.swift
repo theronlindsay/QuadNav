7f21a049f5bbe0bbec5aeaba9cb98f262d00c43a
devilcry3 <78716116+devilcry3@users.noreply.github.com>
2026-03-08 03:33:39 -0600

    Updated Arrow
    Updated arrow to be multi toned with a outline and fill look. These are two different arrows stacked within a zstack to accomplish this

--- QuadNav/3_UI_Views/DirectionArrowView.swift
+++ QuadNav/3_UI_Views/DirectionArrowView.swift
@@ -21,8 +21,12 @@ struct DirectionArrowView: View {
         // VStack stacks the arrow and text vertically.
         VStack {
             
-            // SF Symbol arrow icon provided by Apple.
-            Image(systemName: "arrow.up")
+            
+            ZStack {
+                
+            
+            // SF Symbol arrow outline icon provided by Apple.
+          Image(systemName: "location.north.fill")
                 
                 // Allows the image to resize instead of staying fixed.
                 .resizable()
@@ -38,12 +42,20 @@ struct DirectionArrowView: View {
                 .rotationEffect(.degrees(angle))
                 
                 // Sets the arrow color.
-                .foregroundStyle(.red)
+                .foregroundStyle(.orange)
                 
                 // Adds a small shadow for better visibility.
                 .shadow(radius: 4)
+                
+                Image(systemName: "location.north")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+                    .rotationEffect(.degrees(angle))
+                    .foregroundStyle(.blue)
+                    .shadow(radius: 4)
             
-            
+            }
             // Instruction text displayed under the arrow.
             Text("Follow Arrow")
                 .font(.caption)   // Small caption-style text
