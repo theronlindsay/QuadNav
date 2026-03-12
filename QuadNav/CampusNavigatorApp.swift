0d07bca04e15a9274b059e983d29115142e29ca3
theronlindsay <theronlindsay@protonmail.com>
2026-03-11 19:03:13 -0700

    Fixed Location Monitor Notifications

--- QuadNav/CampusNavigatorApp.swift
+++ QuadNav/CampusNavigatorApp.swift
@@ -4,9 +4,16 @@ import SwiftUI
 
 @main
 struct CampusNavigatorApp: App {
+    
+    @State private var monitor = LocationMonitor()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(monitor)
+                .task {
+                    await monitor.startLocationMonitoring()
+                }
         }
     }
 }
